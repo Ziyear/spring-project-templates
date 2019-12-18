0db89e67cb9c80df0c392e3fad74e5b1f8a469ab
SuperZiyear <SuperZiyear@163.com>
2019-12-18 20:33:59 +0800

    初添加一个spi

---
+++ ziyear-start/src/main/java/com/ziyear/common/DateUtils.java
@@ -0,0 +1,10 @@
+package com.ziyear.common;
+
+import java.util.Date;
+
+public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
+
+    public static Date getNow() {
+        return new Date();
+    }
+}
