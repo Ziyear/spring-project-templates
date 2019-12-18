6b7626cd6c3931e11a7111fe7d58188f8063c785
SuperZiyear <SuperZiyear@163.com>
2019-12-18 20:31:18 +0800

    初添加一个spi

--- ziyear-start/src/main/java/com/ziyear/common/StringUtils.java
+++ ziyear-start/src/main/java/com/ziyear/common/StringUtils.java
@@ -1,4 +1,10 @@
 package com.ziyear.common;
 
+import java.util.UUID;
+
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
+
+    public static String randomString() {
+        return UUID.randomUUID().toString();
+    }
 }
